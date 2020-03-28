0847d76eefef188bd40b20e41a73c3269d752c7d
Asena Kars <karse27@gmail.com>
2020-03-28 22:45:41 +0200

    tema blog

---
+++ 2020:03:28/tema_blog/tema_blog/blog_app/urls.py
@@ -0,0 +1,14 @@
+from . import views
+from django.urls import path, include
+
+
+urlpatterns = [
+    path('', views.PostList.as_view(), name='home'),
+
+    # path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('<slug:slug>/', views.post_detail, name='post_detail'),
+
+    # path('contact/', views.ContactPageView.as_view(), name='contact')
+    path('contact/', views.contact, name='contact')
+
+]
